602f9d573404d5b386bcd2a8dea63758be750688
Pauloh2206 <106989125+Pauloh2206@users.noreply.github.com>
2025-05-12 17:27:32 -0300

    Update Install_xui.sh

--- Install_xui.sh
+++ Install_xui.sh
@@ -4,11 +4,12 @@ echo "----------------------------------------------------------------------"
 echo "(telegram: @sunmodls) :)"
 echo "Tutorial em vídeo das instruções abaixo ⬇️ (Recomendado)"
 echo "Copia o link: https://drive.google.com/file/d/16m_-uWCpm8dYgXMVCwGroOQtmwPmqQs8/view"
-echo "Pós instalação segue com o vídeo"
 echo "----------------------------------------------------------------------"
+
 sleep "7"
 echo "Iniciando a instalação do painel 3x-ui..."
 sleep "4"
 bash <(curl -Ls https://raw.githubusercontent.com/Pauloh2206/PainelXHTTP/refs/heads/main/install.sh)
 echo "Script de instalação fornecido com créditos para: (telegram: @sunmodls)"
+echo "Pós instalação segue com o vídeo"
 sleep "2"
