3ef018453cbb31d797d189aa44297ee0058f2dd5
Pauloh2206 <106989125+Pauloh2206@users.noreply.github.com>
2025-05-20 18:05:06 -0300

    Update install_xui.sh

--- install_xui.sh
+++ install_xui.sh
@@ -5,23 +5,27 @@ CYAN='\033[0;36m'
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m'
-echo -e apt update && apt upgrade -y
+
+# Adicionando os comandos de atualizaÃ§Ã£o do sistema
+echo -e "${GREEN}Atualizando o sistema...${NC}"
+apt update && apt upgrade -y
+
 echo -e "${YELLOW}------------------------------------------------------------${NC}"
 echo -e "${CYAN}(telegram: @sunmodls) :)${NC}"
-echo -e "${CYAN}Tutorial em vídeo das instruções abaixo ⬇️ (Recomendado)${NC}"
+echo -e "${CYAN}Tutorial em vÃ­deo das instruÃ§Ãµes abaixo â¬‡ï¸ (Recomendado)${NC}"
 echo -e "${CYAN}Tutorial: https://short-link.me/12goW${NC}"
 echo -e "${YELLOW}------------------------------------------------------------${NC}"
 
 sleep "8"
-echo -e "${GREEN}Iniciando a instalação do painel 3x-ui...${NC}"
+echo -e "${GREEN}Iniciando a instalaÃ§Ã£o do painel 3x-ui...${NC}"
 sleep "4"
 
 bash <(curl -Ls https://raw.githubusercontent.com/Pauloh2206/PainelXHTTP/refs/heads/main/install.sh)
 echo -e "${YELLOW}------------------------------------------------------------${NC}"
 echo -e "${CYAN}Script e tutorial fornecido${NC}"
 echo -e "${CYAN}Telegram: BY: @sunmodls${NC}"
-echo -e "${RED}PÓS INSTALAÇÃO SEGUE COM O VÍDEO${NC}"
+echo -e "${RED}PÃ“S INSTALAÃ‡ÃƒO SEGUE COM O VÃDEO${NC}"
 echo -e "${YELLOW}PEGA AS CREDENCIAIS DE ACESSO DA URL${NC}"
-echo -e "${YELLOW}VOCÊ VERÁ NO VÍDEO${NC}"
+echo -e "${YELLOW}VOCÃŠ VERÃ NO VÃDEO${NC}"
 echo -e "${YELLOW}------------------------------------------------------------${NC}"
 sleep "2"
